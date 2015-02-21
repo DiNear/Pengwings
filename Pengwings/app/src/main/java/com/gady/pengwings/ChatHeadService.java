7d1375da9fb44a1474ded23493f9bcbba1c6bce8
DiNear <andyhuangk@hotmail.com>
2015-02-21 14:56:18 -0500

    Got the chat head moving working

--- Pengwings/app/src/main/java/com/gady/pengwings/ChatHeadService.java
+++ Pengwings/app/src/main/java/com/gady/pengwings/ChatHeadService.java
@@ -17,13 +17,16 @@ public class ChatHeadService extends Service {
 
     private WindowManager windowManager;
     private ImageView chatHead;
+    private WindowManager.LayoutParams params;
 
-    @Override public IBinder onBind(Intent intent) {
+    @Override
+    public IBinder onBind(Intent intent) {
         // Not used
         return null;
     }
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
@@ -31,7 +34,7 @@ public class ChatHeadService extends Service {
         chatHead = new ImageView(this);
         chatHead.setImageResource(R.drawable.ic_launcher);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
@@ -44,31 +47,31 @@ public class ChatHeadService extends Service {
 
         windowManager.addView(chatHead, params);
 
-//        chatHead.setOnTouchListener(new View.OnTouchListener() {
-//            private int initialX;
-//            private int initialY;
-//            private float initialTouchX;
-//            private float initialTouchY;
-//
-//            @Override public boolean onTouch(View v, MotionEvent event) {
-//                switch (event.getAction()) {
-//                    case MotionEvent.ACTION_DOWN:
-//                        initialX = params.x;
-//                        initialY = params.y;
-//                        initialTouchX = event.getRawX();
-//                        initialTouchY = event.getRawY();
-//                        return true;
-//                    case MotionEvent.ACTION_UP:
-//                        return true;
-//                    case MotionEvent.ACTION_MOVE:
-//                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
-//                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
-//                        windowManager.updateViewLayout(chatHead, params);
-//                        return true;
-//                }
-//                return false;
-//            }
-//        });
+        chatHead.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(chatHead, params);
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
 
